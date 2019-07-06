358e90dccb1429d438e7cf9a93a30a48b4551a4a
Richard Jeremias <41641596+rijema@users.noreply.github.com>
2019-07-06 05:06:46 -0300

    Add files via upload

--- Exercises/LearningFunctionalKotlin.kt
+++ Exercises/LearningFunctionalKotlin.kt
@@ -1,41 +1,71 @@
-abstract class List()
-data class Node( val info:Int,val prox:List):List() 
-object Nil:List()
+Inicialmente, se quero usar recursividade, tenho que trabalhar com essas linhas de código também
+abstract class List
 
- /*
-fun calcularSomaElementos(l:List):Int{
-    if(l.info == null){
-        0
-    }
-    else {
-        l.info + calcularSomaElementos(l.prox)
-    }
+data class Node(val info: Int, val prox: List): List()
+
+object Nil: List()
+
+//Remoção de pares de uma lista
+fun removePares(l:List?):List = when(l){
+    is Node -> (if (l.info%2!=0){Node(l.info,removePares(l.prox))}
+                else{removePares(l.prox)})
+    else -> Nil
+ 
 }
- */
 
+//Calcular soma de Elementos de uma lista
 fun calcularSomaElementosNode(l:List):Int = when(l){
     is Node -> l.info + calcularSomaElementosNode(l.prox)
     else -> 0
 }
 
-fun removePares(l:List):List = when (l){
-    is Node -> if((l.info%2)!=0){
-                Node(l.info,removePares(l.prox))
-            } else{removePares(l.prox)}
+
+//Calcula maiores que 10 em uma lista
+fun maiorDez(l:List?):Int = when(l){
+    is Node -> auxCalculaMaior(l,0)
+    else -> 0
+}
+
+    fun auxCalculaMaior(l:List?,v:Int):Int = when(l){
+        is Node -> (if(l.info>=10){auxCalculaMaior(l.prox,v+l.info)} else{auxCalculaMaior(l.prox,v)})
+        else -> v
+            
+    }
+
+//Insere elemento em uma determinada posição
+fun insereElemento(l:List?,elemento:Int,posicao:Int):List = when(l){
+    is Node -> auxInsereElemento(l,elemento,posicao,0)
     else -> Nil
 }
-/* 
-fun contem(l:List,l2:List):Boolean = when(l){
-         is Node ->   if(l.info == null && l2.info != null){ false
-            } else if(l.info == null && l2.info == null){ true }
-        else -> if(l.info == l2.info){
-                    contem(l.prox,l2.prox)
-                    } else{
-                    contem(l.prox,l2)
-                     }
-            }
 
 
+    fun auxInsereElemento(l:List?,elemento:Int,posicao:Int,contador:Int):List = when(l) {
+        //verificar se é  pra sobrepor mesmo
+        is Node -> (if(contador==posicao){
+                    Node(elemento,auxInsereElemento(l.prox,elemento,posicao,contador+1))}
+                    else{
+                        Node(l.info,auxInsereElemento(l.prox,elemento,posicao,contador+1))
+                    })
+        else -> Nil
+                    
+    }
+
+
+//Verifica a quantidade de vezes um elemento específico se encontra na Lista
+fun contarOcorrencias(l:List?,valor:Int):Int = when(l){
+    is Node -> auxContarOcorrencias(l,valor,0)
+    else -> 0
+}
+
+    fun auxContarOcorrencias(l:List?,valor:Int,contador:Int):Int = when(l){
+        is Node -> (if(l.info==valor){auxContarOcorrencias(l.prox,valor,contador+1)}
+                    else{auxContarOcorrencias(l.prox,valor,contador)}
+        )
+        else -> contador
+    }
+
+//Verificar se uma subsequencia está contida em uma sequencia
+//Obs: Há erro por falta de verificação se o l2 é do tipo Node.
 fun contem(l:List,l2:List):Boolean = when(l){
         is Node -> if(l.info == l2.info){
                         contem(l.prox,l2.prox)
@@ -44,49 +74,5 @@ fun contem(l:List,l2:List):Boolean = when(l){
                      }
          else ->  if(l2 == null){true} else{false}
         
-            }
+    }
 
-fun contem(l:List,l2:List):Boolean {
-        if(l==List || l2==List){
-            if(l.info == l2.info){
-                        contem(l.prox,l2.prox)
-                    } else{
-                    contem(l.prox,l2)
-                     }
-        } 
-         else{ if(l2 == null){true} else{false}}
-        
- }
-
-  fun contem(l:List,l2:List):Boolean{
-        if(l==List || l2==List){
-            if(l.info == l2.info){
-                        contem(l.prox,l2.prox)
-                    } else{
-                    contem(l.prox,l2)
-                     }
-        } 
-         else{ if(l2 == null){true} else{false}}
-        
- }
-*/
- fun contem(l:List,l2:List):Boolean = when(l2){
-
-     is Node -> when(l){
-            is Node -> if(l.info == l2.info){
-                        contem(l.prox,l2.prox)
-                    } else{
-                    contem(l.prox,l2)
-                     }
-            else ->  if(l2 == null){true} else{false}
-            }
-     else -> false
- }
-
-fun main(){
-    val l1 = Node(1,Node(2,Node(3,Node(4,Nil))))
-    println("Remove pares:" + removePares(l1))
-    println("Calcula Soma:" + calcularSomaElementosNode(l1))
-    val l2 = Node(2,Node(3,Nil))
-    println("Verificar subsequencia:" + contem(l1,l2))
-}
