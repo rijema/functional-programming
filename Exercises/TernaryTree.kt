247d279bbcf02179191d0ed7c01f3dfe8f4d5574
Richard Jeremias <41641596+rijema@users.noreply.github.com>
2019-07-10 12:59:29 -0300

    Add files via upload

--- Exercises/TernaryTree.kt
+++ Exercises/TernaryTree.kt
@@ -3,7 +3,7 @@ abstract class Arvore
 data class NoArvore(val info: Int,val esquerda: Arvore, val meio: Arvore, val  direita: Arvore):Arvore()
 object N: Arvore()
 
-//Calcula a altura dos Nos de uma árvore Ternária, ou seja, retornará o maior caminho da árvore
+//(depth-first search)Calcula a altura dos Nos de uma árvore Ternária, ou seja, retornará o maior caminho da árvore
 fun calcularAltura(arvoreUm:Arvore):Int = when (arvoreUm){
     is NoArvore ->  {
                 val alturaEsquerda = calcularAltura(arvoreUm.esquerda)
@@ -17,6 +17,49 @@ fun calcularAltura(arvoreUm:Arvore):Int = when (arvoreUm){
     else -> -1
 }
 
+//(breadth-first search by level) Calcula quantos nós existem em um determinado nível daquela árvore
+fun calcularQuantidadeNosNivel(arvoreUm:Arvore,nivel:Int):Int = when (arvoreUm){
+    is NoArvore -> {
+                if(nivel==0){
+                        1
+                } else{
+
+            val alturaEsquerda = 0 + calcularQuantidadeNosNivel(arvoreUm.esquerda,nivel-1)
+            val alturaMeio = 0 + calcularQuantidadeNosNivel(arvoreUm.meio,nivel-1)
+            val alturaDireita = 0 + calcularQuantidadeNosNivel(arvoreUm.direita,nivel-1)
+            
+            alturaDireita+alturaEsquerda+alturaMeio
+
+            }  
+    }  
+    else -> {
+         0
+    }
+}
+
+//Verifica se em cada nível há um limite de 2^(nivel) nós 
+fun verificarExponencialidade(arvoreUm:Arvore):Boolean {
+    return auxiliarExponencialidade(arvoreUm,calcularAltura(arvoreUm),0)
+}
+
+fun auxiliarExponencialidade(arvoreUm:Arvore,loop:Int,nivel:Int):Boolean = when{
+    (nivel <= loop) -> {if(calcularQuantidadeNosNivel(arvoreUm,nivel) > baseDois(nivel)){
+                             false
+                     } else {
+                        auxiliarExponencialidade(arvoreUm,loop,nivel+1)
+                    }
+               }
+    else -> true
+                  
+}
+
+//Calcula a potência de 2 a base do número de entrada
+fun baseDois(numero:Int):Int {
+    if(numero == 0){
+        return 1
+    } 
+    return 2*(baseDois(numero-1))
+    }
 
 //Criará uma lista com a árvore associada, dissertando os nós.
 fun criarLista(arvoreUm:Arvore):Arvore = when(arvoreUm){
@@ -80,16 +123,26 @@ fun verificarIgualdadeSimples(arvoreUm:Arvore,arvoreDois:Arvore):Boolean {
     return false
 }
 
+
 //Visualização de execução das funções. Para melhor visualização, indico apenas um por compilação.
 fun main(){
-    //val one = NoArvore(1,NoArvore(2,NoArvore(22,N,N,N),NoArvore(222,N,N,N),NoArvore(2222,N,N,N)),NoArvore(3,NoArvore(33,N,N,N),NoArvore(333,N,N,N),NoArvore(3333,N,N,N)),NoArvore(4,NoArvore(44,N,N,N),NoArvore(444,N,N,N),NoArvore(4444,N,NoArvore(5,N,N,N),N)))
-    //println(calcularAltura(one))
-    
+    val one = NoArvore(1,NoArvore(2,NoArvore(22,N,N,N),NoArvore(222,N,N,N),NoArvore(2222,N,N,N)),NoArvore(3,NoArvore(33,N,N,N),NoArvore(333,N,N,N),NoArvore(3333,N,N,N)),NoArvore(4,NoArvore(44,N,N,N),NoArvore(444,N,N,N),NoArvore(4444,N,NoArvore(5,N,N,N),N)))
+    println(calcularAltura(one))
+    println(calcularQuantidadeNosNivel(one,1))
+    println(">>>>>" + baseDois(4))
+    println("Verificando Exponencialidade: " + verificarExponencialidade(one))
+    //val exponencial = NoArvore(1,NoArvore(2,NoArvore(22,N,N,N),N,N),NoArvore(3,NoArvore(33,N,N,N),NoArvore(333,N,N,N),NoArvore(3333,N,N,N)),NoArvore(4,NoArvore(44,N,N,N),NoArvore(444,N,N,N),NoArvore(4444,N,NoArvore(5,N,N,N),N)))
+    val exponencial2 =  NoArvore(1,NoArvore(2,NoArvore(22,N,N,N),N,N),NoArvore(45,NoArvore(23,N,N,N),N,N),NoArvore(76,NoArvore(67,N,N,N),N,N))
+    println("Verificando Exponencialidade: " + verificarExponencialidade(exponencial2))
+    val exponencial3 =  NoArvore(1,NoArvore(2,NoArvore(22,N,N,N),N,N),N,NoArvore(76,NoArvore(67,N,N,N),N,N))
+    println("Verificando Exponencialidade: " + verificarExponencialidade(exponencial3))
+
+    /*
     val two = NoArvore(1,NoArvore(4,NoArvore(44,N,N,N),NoArvore(555,N,N,N),NoArvore(22,N,N,N)),NoArvore(31,NoArvore(35,N,N,N),NoArvore(673,N,N,N),NoArvore(3,N,N,N)),NoArvore(42,NoArvore(8,N,N,N),NoArvore(88,N,N,N),NoArvore(8888,N,NoArvore(9,N,N,N),N)))
     val three = NoArvore(1,NoArvore(4,NoArvore(44,N,N,N),NoArvore(555,N,N,N),NoArvore(22,N,N,N)),NoArvore(31,NoArvore(35,N,N,N),NoArvore(673,N,N,N),NoArvore(3,N,N,N)),NoArvore(42,NoArvore(8,N,N,N),NoArvore(88,N,N,N),NoArvore(8888,N,NoArvore(9,N,N,N),N)))
     val four = NoArvore(0,NoArvore(4,NoArvore(92,N,N,N),NoArvore(555,N,N,N),NoArvore(22,N,N,N)),NoArvore(31,NoArvore(35,N,N,N),NoArvore(673,N,N,N),NoArvore(3,N,N,N)),NoArvore(42,NoArvore(8,N,N,N),NoArvore(88,N,N,N),NoArvore(8888,N,NoArvore(9,N,N,N),N)))
 
-    /* 
+    
    println(concatenarDuplo(one,two))
    println(criarListaEspecifica(two,2))
    println(verificarIgualdade(two,three))
@@ -97,7 +150,7 @@ fun main(){
    println(verificarIgualdade(two,four))
    println(verificarIgualdadeSimples(two,three))
    println(verificarIgualdadeSimples(two,four)) 
-    */
+ */
     }
 
 
